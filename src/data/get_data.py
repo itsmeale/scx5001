5a742ce9726812778b8302aa3fd4667574967a1b
Alexandre Farias <0800alefarias@gmail.com>
2021-10-01 11:30:00 -0300

    Refactor extract feminicide data method

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -2,20 +2,24 @@ import pandas as pd
 
 from sspdata.datasets.feminicidio import extrair_feminicidios
 
-
-years = list(range(2015, 2022))
-months = list(range(1, 13))
+YEARS = list(range(2015, 2022))
+MONTHS = list(range(1, 13))
 
 dataframes = list()
 
-for year in years:
-    for month in months:
-        try:
-            dataframes.append(extrair_feminicidios(year, month))
-        except Exception as e:
-            print(f"Could not extract data for y: {year}, m: {month}")
-        print(f"{year}, {month} finished")
+
+def extract_feminicide_data():
+    for year in YEARS:
+        for month in MONTHS:
+            try:
+                dataframes.append(extrair_feminicidios(year, month))
+            except Exception:
+                print(f"Could not extract data for y: {year}, m: {month}")
+            print(f"{year}, {month} finished")
+
+    unified_dataframe = pd.concat(dataframes)
+    unified_dataframe.to_csv("data/raw/feminicide_ssp.csv")
 
 
-unified_dataframe = pd.concat(dataframes)
-unified_dataframe.to_csv("data/raw/feminicide_ssp.csv")
+if __name__ == '__main__':
+    extract_feminicide_data()
